864d9a01876e79b341d0b19f81a509b227e36b57
Narayan2416 <phnarayanamoorthy@gmail.com>
2026-09-19 18:34:20 +0530

    added dotenv

--- api/services.py
+++ api/services.py
@@ -7,7 +7,7 @@ bp=Blueprint("apis",__name__)
 @bp.route("/api/movies/play/<int:id>")
 def video(id):
     VIDEO_PATH = getMovieById(id).get("movie_path")
-    #print(VIDEO_PATH,"has path?")
+    print(VIDEO_PATH,"has path?")
 
     if not os.path.exists(VIDEO_PATH):
         return "Video not found", 404
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_cors import CORS
 from api.services import bp as service_bp
+from dotenv import load_dotenv
 import os
 
+load_dotenv()
 
 app = Flask(__name__)
-CORS(app,origins=os.getenv("frontend_url"))
+frontend_urls = os.getenv("frontend_url", "").split(",")
+
+CORS(app, origins=frontend_urls)
 
 
 app.register_blueprint(service_bp)
